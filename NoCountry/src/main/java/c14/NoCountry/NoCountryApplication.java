0727fa3a70702ccbbe161d1d0504269d29c41db9
Carlos Barrera Aleman <115600193+devpcarlos@users.noreply.github.com>
2024-04-02 13:52:46 -0500

    mi primer commit prara

---
+++ NoCountry/src/main/java/c14/NoCountry/NoCountryApplication.java
@@ -0,0 +1,13 @@
+package c14.NoCountry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoCountryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoCountryApplication.class, args);
+	}
+
+}
---
+++ NoCountry/src/test/java/c14/NoCountry/NoCountryApplicationTests.java
@@ -0,0 +1,13 @@
+package c14.NoCountry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoCountryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
