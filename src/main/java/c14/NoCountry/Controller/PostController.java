3642229ab5a626345591f71ce54f67ddd139ef1b
Carlos Barrera Aleman <115600193+devpcarlos@users.noreply.github.com>
2024-04-03 16:58:52 -0500

    corrigiendo @postMapping a @GetMapping

--- src/main/java/c14/NoCountry/Controller/PostController.java
+++ src/main/java/c14/NoCountry/Controller/PostController.java
@@ -3,6 +3,7 @@ package c14.NoCountry.Controller;
 import c14.NoCountry.Service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import c14.NoCountry.Entity.Post;
@@ -21,7 +22,7 @@ public class PostController {
        return ResponseEntity.ok(pse.save(pss));
     }
 
-    @PostMapping("/all-post")
+    @GetMapping("/all-post")
     public ResponseEntity<?> getPost() {
         return ResponseEntity.ok(pse.findByAll());
     }
