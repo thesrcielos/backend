09c31c5d4e8d8652df73a257b72db06adf0e4017
Carlos Barrera Aleman <115600193+devpcarlos@users.noreply.github.com>
2024-04-04 12:22:35 -0500

    agregando el metodo findByName

--- src/main/java/c14/NoCountry/Repository/PostRepository.java
+++ src/main/java/c14/NoCountry/Repository/PostRepository.java
@@ -3,5 +3,9 @@ package c14.NoCountry.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import c14.NoCountry.Entity.post;
 
+import java.util.List;
+
 public interface PostRepository extends JpaRepository<post, Long> {
+
+    List<post> findByName(String name);
 }
