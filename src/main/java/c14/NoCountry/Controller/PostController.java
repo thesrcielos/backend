d832687423dd12220b30ea0fcadfff5eb64373e5
Carlos Barrera Aleman <115600193+devpcarlos@users.noreply.github.com>
2024-04-04 12:23:56 -0500

    agregando el controlador que va a traer o filtrar por el nombre

--- src/main/java/c14/NoCountry/Controller/PostController.java
+++ src/main/java/c14/NoCountry/Controller/PostController.java
@@ -4,9 +4,12 @@ import c14.NoCountry.Service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import c14.NoCountry.Entity.post;
+
+import java.util.List;
 
 @RestController
 public class PostController {
@@ -26,4 +29,15 @@ public class PostController {
     public ResponseEntity<?> getPost() {
         return ResponseEntity.ok(pse.findByAll());
     }
+
+    @GetMapping("/getpost/{name}")
+    public ResponseEntity<?> getName(@PathVariable String name) {
+        List<post> posts = pse.findbyName(name);
+        if(posts.isEmpty()) {
+            return ResponseEntity.badRequest().body("No existe el nombre del póst");
+        }
+        return ResponseEntity.ok(posts);
+    }
+
 }
+
