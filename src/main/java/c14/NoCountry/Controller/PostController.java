e93267c674c060c47a2f78abeff8a58867345f85
Carlos Barrera Aleman <115600193+devpcarlos@users.noreply.github.com>
2024-04-04 14:31:12 -0500

    corrigiendo errores y mejorando las validaciones

--- src/main/java/c14/NoCountry/Controller/PostController.java
+++ src/main/java/c14/NoCountry/Controller/PostController.java
@@ -1,15 +1,15 @@
 package c14.NoCountry.Controller;
 
 import c14.NoCountry.Service.PostService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 import c14.NoCountry.Entity.post;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class PostController {
@@ -17,11 +17,21 @@ public class PostController {
     @Autowired
     private PostService pse;
 
+//// la anotacion @valid se usa para activar la validacion de entrada para post
+//    el parametro BindingResult lo usamos para manejar las excepciones de validacion
+
     @PostMapping("/save-post")
-    public ResponseEntity<?> savePost(post pss){
-        if (pss==null){
-            return ResponseEntity.badRequest().body("No puede haber datos vacios");
+    public ResponseEntity<?> savePost(@Valid @RequestBody post pss, BindingResult result){
+
+        if (result.hasErrors()) {
+            List<String> errorMessages = result.getAllErrors()
+                    .stream()
+                    .map(error -> error.getDefaultMessage())
+                    .collect(Collectors.toList());
+// si hay errores de validacion, se devuelven en una lista en una utilizando lo siguiente:
+            return ResponseEntity.badRequest().body(errorMessages);
         }
+
        return ResponseEntity.ok(pse.save(pss));
     }
 
@@ -36,6 +46,7 @@ public class PostController {
         if(posts.isEmpty()) {
             return ResponseEntity.badRequest().body("No existe el nombre del póst");
         }
+
         return ResponseEntity.ok(posts);
     }
 
